d9d069614d2755b521f3c18a4407c342b0a69ae2
niraj-creator <nirajhamom@gmail.com>
2025-03-13 10:19:10 +0530

    code1

---
+++ double/dou.java
@@ -0,0 +1,7 @@
+public class dou {
+    
+    public static void main(String[] args) {
+        double number = 5.678;
+        System.out.println(number);
+    }
+}
--- double/doub.java
+++
@@ -1,6 +0,0 @@
-public class doub {
-    public static void main(String[]args){
-        double number = 5.67;
-        System.out.println(number);
-    }
-}
