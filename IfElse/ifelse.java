9d0fa5e41484feb102ae90ab5c0ac1799862cc8b
niraj-creator <nirajhamom@gmail.com>
2025-03-13 13:44:32 +0530

    als

---
+++ IfElse/ifelse.java
@@ -0,0 +1,13 @@
+package IfElse;
+ 
+ public class ifelse {
+     public static void main(String[] args) {
+         if(1<8){
+             System.out.println("True");
+         }
+         else{
+             System.out.println("false");
+         }
+     }
+     
+ }
