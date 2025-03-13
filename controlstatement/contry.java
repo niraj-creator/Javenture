e9d7945994be9cc537e95ff7860875d72a523ace
niraj-creator <nirajhamom@gmail.com>
2025-03-13 14:11:58 +0530

    bel

---
+++ controlstatement/contry.java
@@ -0,0 +1,9 @@
+package controlstatement;
+
+public class contry {
+    public static void main(String[] args) {
+        for(int i=6;i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ evenno/bel.java
@@ -0,0 +1,11 @@
+package evenno;
+
+public class bel {
+    public static void main(String[] args) {
+        for(int i=11;i<=20;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
