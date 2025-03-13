39628da18cf25adbee01e4dd1521aa03fd1f8932
niraj-creator <nirajhamom@gmail.com>
2025-03-13 15:46:14 +0530

    thi

---
+++ ballco/ballColor.java
@@ -0,0 +1,8 @@
+package ballco;
+ 
+ public class ballColor{
+     public static void main(String args[]){
+         String a[]={"black","blue","green"};
+         System.out.println(a[2]);
+     }
+ }
