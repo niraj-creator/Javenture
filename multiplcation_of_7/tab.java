e7688a5fb15e821f1b3cbc915865ac68a2b2a548
niraj-creator <nirajhamom@gmail.com>
2025-03-14 10:06:21 +0530

    naorem_alex2

---
+++ multiplcation_of_7/tab.java
@@ -0,0 +1,11 @@
+package multiplcation_of_7;
+
+public class tab {
+    public static void main(String args[]){
+        int i=1;
+        while(i<=10){
+            System.out.println("7 * "+i+" = "+7*i);
+            i++;
+        }
+    }
+}
---
+++ while_loop2/whk.java
@@ -0,0 +1,11 @@
+package while_loop2;
+
+public class whk {
+    public static void main(String args[]){
+        int i=90;
+        while(i<99){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
