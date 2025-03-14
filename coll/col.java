54a931f8f8d4333fb54199d2e13ddaf75961282c
niraj-creator <nirajhamom@gmail.com>
2025-03-14 09:42:10 +0530

    alex

---
+++ coll/col.java
@@ -0,0 +1,14 @@
+package coll;
+
+import java.util.*;
+  public class col{
+      public static void main(String args[]){
+          Scanner input =new Scanner(System.in);
+          System.out.println("Enter the number");
+          int num1=input.nextInt();
+          int num2=input.nextInt();
+          int num3= num1+num2;
+          System.out.println(num3);
+  
+      }
+  }
