7032a4292c645778cc258e29223645183359dcf0
niraj-creator <nirajhamom@gmail.com>
2025-03-14 11:43:02 +0530

    naorem_alex6

---
+++ strg/stgh.java
@@ -0,0 +1,11 @@
+package strg;
+
+import string.str;
+import java.util.Scanner;
+public class stgh {
+    public static void main(String args[]){
+        String s1 = "Hello";
+        System.out.println(s1.charAt(0));
+        System.out.println(s1.charAt(1));
+    }
+}
